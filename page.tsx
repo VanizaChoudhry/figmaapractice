38b5db19a0552437ac75f5d0095fb8b8577c77e9
VanizaChoudhry <165427729+VanizaChoudhry@users.noreply.github.com>
2024-12-06 07:27:47 -0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+// pages/index.js
+import Footer from '../app/footer/page';
+
+export default function Home() {
+    return (
+        <div>
+            {/* Your main content goes here */}
+            <Footer />
+        </div>
+    );
+}
